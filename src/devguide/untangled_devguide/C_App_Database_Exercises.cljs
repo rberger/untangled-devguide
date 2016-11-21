a1cf35fb7ad83059231de5c87c784765a3f7b029
Robert J. Berger <rberger@ibd.com>
2016-11-21 00:07:54 -0800

    Completed C Exercises

--- src/devguide/untangled_devguide/C_App_Database_Exercises.cljs
+++ src/devguide/untangled_devguide/C_App_Database_Exercises.cljs
@@ -6,19 +6,39 @@
             [cljs.reader :as r]))
 
 (def cars-table
-  {
                                         ; TODO (exercise 1): Add a :cars/by-id table
+  {
+   :cars/by-id {
+                1 { :make "Nissan" :model "Leaf" }
+                2 { :make "Dodge" :model "Dart" }
+                3 { :make "Ford" :model "Mustang" }}
    })
 
 (def favorites
-                                        ; TODO (exercise 2): merge your `cars-table` from above here
+                                      ; TODO (exercise 2): merge your `cars-table` from above here
   {
+    :cars/by-id {
+                 1 { :make "Nissan" :model "Leaf" }
+                 2 { :make "Dodge" :model "Dart" }
+                 3 { :make "Ford" :model "Mustang" }}
                                         ; TODO (exercise 2): Add a :favorite-car key that points to the Nissan Leaf via an ident
+   :favorite-car [ :cars/by-id 1 ]
    })
 
 (def ex3-uidb
   {
                                         ; TODO (exercise 3): Add tables. See exercise text.
+   :tools/by-id {
+                 1 {:label "Cut"}
+                 2 {:label "Copy"}
+                 }
+   :data/by-id {
+                5 {:x 1 :y 3}
+                }
+   :main-panel {
+                :toolbar {:tools[[:tools/by-id 1] [:tools/by-id 2]]}
+                :canvas {:data  [[:data/by-id 5]]}
+                }
    })
 
 (defcard-doc
